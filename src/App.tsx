4f304e8b64c7dc25cb9c837b49fd2c1f740dd635
osephson <osefson@gmail.com>
2023-06-23 18:15:06 +0200

    add: Roboto fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import HotelList from "./containers/HotelList";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <HotelList />
     </div>
   );
