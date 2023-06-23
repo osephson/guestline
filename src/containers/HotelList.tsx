5f94758ad42615dc16ed9a539d16d954478adc1f
osephson <osefson@gmail.com>
2023-06-23 18:22:51 +0200

    update: HotelList

--- src/containers/HotelList.tsx
+++ src/containers/HotelList.tsx
@@ -1,21 +1,48 @@
 import { useState, useEffect } from "react";
+import { Container, Grid } from "@mui/material";
 
-import { HotelList as Hotels } from "../interfaces/hotels";
-import { fetchHotels } from "../api";
+import { IHotelList } from "../interfaces/hotels";
+import { fetchHotels, fetchRooms } from "../api";
+
+import HotelItem from "./HotelItem";
+import { Filter } from "../components";
 
 const COLLECTION_ID = "OBMNG";
 
 const HotelList = () => {
-  const [hotels, setHotels] = useState<Hotels>([]);
+  const [hotels, setHotels] = useState<IHotelList>([]);
 
   useEffect(() => {
     (async function () {
       const response = await fetchHotels(COLLECTION_ID);
-      setHotels(response.data);
+      const hotels = response.data;
+
+      const fetchRoomsPromises = hotels.map((h) =>
+        fetchRooms(COLLECTION_ID, h.id)
+      );
+      const roomsResponse = await Promise.all(fetchRoomsPromises);
+
+      hotels.forEach((h, index) => {
+        h.rooms = roomsResponse[index].data.rooms;
+      });
+
+      setHotels(hotels);
     })();
   }, []);
 
-  return <h1>Hotel List</h1>;
+  return (
+    <Container maxWidth="md">
+      <Filter />
+      <Grid container rowSpacing={2}>
+        {!!hotels.length &&
+          hotels.map((h, index) => (
+            <Grid key={index} item xs={12}>
+              <HotelItem data={h} />
+            </Grid>
+          ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default HotelList;
