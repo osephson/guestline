54d751594887714c2a71147c30c8746d69043aa5
osephson <osefson@gmail.com>
2023-06-23 18:16:45 +0200

    add: interfaces

--- src/interfaces/hotels.ts
+++ src/interfaces/hotels.ts
@@ -1,13 +1,34 @@
+export interface IImage {
+  url: string;
+  alt?: string;
+}
+
 export interface IHotel {
   id: string;
   name: string;
   description: string;
   address1: string;
   address2: string;
-  images: Array<{
-    url: string;
-  }>;
+  images: Array<IImage>;
   starRating: number;
+  rooms?: Array<IRoom>;
+}
+
+export interface IRoom {
+  id: string;
+  name: string;
+  shortDescription: string;
+  longDescription: string;
+  occupancy: {
+    maxAdults: number;
+    maxChildren: number;
+    maxOverall: number;
+  };
+  images: Array<IImage>;
+}
+
+export interface IRoomsResponse {
+  rooms: Array<IRoom>;
 }
 
 export interface IHotelList extends Array<IHotel> {}
